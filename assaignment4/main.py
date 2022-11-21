5b8b66867cc0b8893338d2cd183ab86cf7e50cb2
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-11-21 10:02:27 +0200

    finished the dfs

--- assaignment4/main.py
+++ assaignment4/main.py
@@ -1,61 +1,85 @@
 import random
 
 
-def DFS(value_matrix: list[list]) -> list:
+def is_optimal(v, value_m) -> bool:
     """
-    run until until get to leaf
-    if leaf is optimal then stop run and return the path of dividing the items
-    not optimal then:
-        if  last leaf in group go one group up and try the others
-        not last leaf in group then try next leaf
-    :param value_matrix: the value matrix for value a player gave to each item
-    :return: optimal divide
+    checks if current state is a proportional divide of values
+    :param v: current state
+    :param value_m: value matrix
+    :return:
     """
-    option_set = set()
-    tree_stack = [[
-        0,  # items divided
-        0,  # value for player 1
-        0,  # value for player 2
-        0,  # num of visits
-        0,  # gave to which player
-    ]]
-    while len(tree_stack) != 0:
-        v = tree_stack.pop()
-        # 1) check if number item in v is equal to number of items
-        # 1.1) not equal then -> create all children of current v and push them into stack and repeat
-        # 1.2) equal then check if optimal
-        # 1.2.1) optimal return path
-        # 1.2.2) not optimal continue running
-        if v[0] == len(value_matrix[0]):
-            print("optimal check")
-            optimal_divide, is_optimal = check_optimal(value_matrix, tree_stack, v)
-            if is_optimal:
-                return optimal_divide
-        if v[3] != len(value_matrix):
-            tree_stack.append([v[0], v[1], v[2], v[3] + 1, v[4]])
-            # index of item to divide
-            item_index = v.__getitem__(0) + 1
-            # first child
-            tree_stack.append([item_index, v[] + value_matrix[v[3]], [item_index - 1], 0, v[3]])
+    div = 1 / len(value_m)
+    for i in range(len(value_m)):
+        player_value_sum = sum(value_m[i])
+        if div > v[i + 1] / player_value_sum:
+            return False
+    return True
 
 
-def check_optimal(value_matrix, tree_stack, v) -> (list, bool):
-    for i in range(len(value_matrix)):
-        # 1/n <= value for player
-        if 1/sum(value_matrix[i]) > v[i+1]/sum(value_matrix[i]):
-            return [], False
-    path = [v[]]
+def check_change(curr, prev):
+    """
+    checks which player value changed and return the index
+    :param curr:
+    :param prev:
+    :return:
+    """
+    for i in range(1, len(prev) - 1):
+        if curr[i] > prev[i]:
+            return i
+
+
+def get_path(tree_stack, curr):
+    """
+    traverses stack to build divide path of items to players
+    :param tree_stack:
+    :param curr: current leaf
+    :return:
+    """
+    path = []
+    prev = tree_stack.pop()
+    while len(tree_stack) > 0 and curr[0] != 0:
+        path.append(check_change(curr, prev))
+        curr = prev
+        prev = tree_stack.pop()
+    return path
+
+
+def dfs_optimal_search(value_m: list[list]) -> list:
+    v = [0, 0, 0, 0]
+    tree_stack = [v]
     while len(tree_stack) > 0:
+        v = tree_stack.pop()
+        # check if node is a leaf and if a leaf then check for optimal solution
+        if v[0] == len(value_m[0]):
+            if is_optimal(v, value_m):
+                path = get_path(tree_stack, v)
+                path.append(v[len(v) - 1] + 1)
+                path.reverse()
+                return path
+        elif v[3] < len(value_m) and v[0] < len(value_m[0]):
+            new_v = [v[0], v[1], v[2], 0]
+            # player index to increase the item value of
+            index = v[3]
+            # increase visit count and push parent node back into stack
+            v[3] = v[3] + 1
+            tree_stack.append(v)
+            # handle the new node to use
+            # increase value of player
+            new_v[index + 1] = new_v[index + 1] + value_m[index][new_v[0]]
+            # increase item count
+            new_v[0] = new_v[0] + 1
+            # reset visit count
+            new_v[3] = 0
+            # add new node to tree stack
+            tree_stack.append(new_v)
+        return []
 
-    return path.reverse(), True
 
-
-a = random.Random().random().__round__(4)
-number_of_items = 10
-value = 1
-root = (
-    0,  # player 1 value
-    0,  # player 2 value
-    0)  #
 if __name__ == '__main__':
+    value_matrix = [
+        [11, 22, 33],
+        [11, 22, 33]
+    ]
+    print(dfs_optimal_search(value_matrix))
+
     print("")
