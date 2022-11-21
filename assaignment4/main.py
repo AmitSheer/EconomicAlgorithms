ebf9d1ff397be2ef1fddf427a31515e37ed7d5f6
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-11-21 11:06:20 +0200

    finished the dfs

--- assaignment4/main.py
+++ assaignment4/main.py
@@ -44,9 +44,11 @@ def get_path(tree_stack, curr):
     return path
 
 
+# ------------------------ Q1 ------------------------------
 def dfs_optimal_search(value_m: list[list]) -> list:
     v = [0, 0, 0, 0]
     tree_stack = [v]
+    all_states = dict()
     while len(tree_stack) > 0:
         v = tree_stack.pop()
         # check if node is a leaf and if a leaf then check for optimal solution
@@ -55,13 +57,14 @@ def dfs_optimal_search(value_m: list[list]) -> list:
                 path = get_path(tree_stack, v)
                 path.append(v[len(v) - 1] + 1)
                 path.reverse()
+                print(all_states)
                 return path
         elif v[3] < len(value_m) and v[0] < len(value_m[0]):
             new_v = [v[0], v[1], v[2], 0]
             # player index to increase the item value of
-            index = v[3]
+            index = v[len(v) - 1]
             # increase visit count and push parent node back into stack
-            v[3] = v[3] + 1
+            v[len(v) - 1] = v[len(v) - 1] + 1
             tree_stack.append(v)
             # handle the new node to use
             # increase value of player
@@ -70,16 +73,40 @@ def dfs_optimal_search(value_m: list[list]) -> list:
             new_v[0] = new_v[0] + 1
             # reset visit count
             new_v[3] = 0
-            # add new node to tree stack
-            tree_stack.append(new_v)
-        return []
+            # tree_stack.append(new_v)
+            if remove_duplicate_states(all_states, new_v) is False:
+                tree_stack.append(new_v)
+    return []
+
+
+# ------------------------ Q2 ------------------------------
+def remove_duplicate_states(states: dict[int, set], curr: list):
+    """
+    check if a state was already visited
+    :param states: all unique states
+    :param curr:
+    :return:
+    """
+    # check if key already exist
+    if states.keys().__contains__(curr[0]):
+        # if div already calculated from in different branch then stop the calculation
+        if states.get(curr[0]).__contains__(tuple(curr[1:len(curr) - 1])):
+            print(f'same curr:{curr}')
+            return True
+        else:
+            # this is a new unique state for this item divide add to divide
+            print(f'not in set curr:{curr}')
+            states.get(curr[0]).add(tuple(curr[1:len(curr) - 1]))
+    else:
+        # create new key in dictionary
+        states[curr[0]] = set()
+        states[curr[0]].add(tuple(curr[1:len(curr) - 1]))
+    return False
 
 
 if __name__ == '__main__':
     value_matrix = [
-        [11, 22, 33],
-        [11, 22, 33]
+        [11, 22, 33, 44, 55],
+        [11, 22, 33, 44, 66]
     ]
     print(dfs_optimal_search(value_matrix))
-
-    print("")
